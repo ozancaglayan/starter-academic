1a4941b13287001c26c1c400fad435a4558e5c78
Ozan Caglayan <ozancag@gmail.com>
2021-02-06 00:01:45 +0000

    update

--- build.sh
+++ build.sh
@@ -6,7 +6,5 @@ popd
 
 cp ../CV/cv.pdf static/media
 git pull
-rm -rf content/publication/*
-git checkout -- content/publication/_index.md
 academic import --bibtex citations.bib --overwrite
 hugo --gc --minify -b https://www.ozancaglayan.com -d /var/www/ozancaglayan
